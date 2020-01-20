5f12e39b2bc147afac00aba42d8034c38250cdda
Dilip Thakre <60089550+rm-dilip@users.noreply.github.com>
2020-01-20 17:15:55 +0530

    Final with color changing Background Divs

--- My_js.js
+++ My_js.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
   	$('#rootwizard').bootstrapWizard();
+
+
+    $('.my_link').click(function(){
+      var i=50;
+      var id=this.id;
+      var c_id=id*10;
+      var color='#337a'+c_id;
+      $('#tab'+id).css('background',color)
+      i++;
+      console.log(color);
+    })
 });
